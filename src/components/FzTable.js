b81a80c8028669e61a8e3a0a307b169ec5299abb
Benson Chen <tumlivein@gmail.com>
2019-09-02 16:55:58 +0800

    CSS 粗刻

---
+++ src/components/FzTable.js
@@ -0,0 +1,174 @@
+import React from "react";
+
+class FzTable extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div className="schedule">
+        <div className="set_off">
+          <ul className="row">
+            <li className="info date">
+              <span>去程</span>
+              <span>回程</span>
+            </li>
+            <li className="info date">12/27(三)</li>
+            <li className="info date">12/28(四)</li>
+            <li className="info date">12/29(五)</li>
+            <li className="info date">12/30(六)</li>
+            <li className="info date">12/31(日)</li>
+            <li className="info date">01/01(一)</li>
+            <li className="info date">01/02(二)</li>
+          </ul>
+        </div>
+        <div className="return">
+          <div className="leftBtn"></div>
+          <div className="rightBtn"></div>
+          <ul className="row">
+            <li className="info date">12/30(六)</li>
+            <li className="info date">12/31(日)</li>
+            <li className="info date">01/01(一)</li>
+            <li className="info date">01/02(二)</li>
+            <li className="info date">01/03(三)</li>
+            <li className="info date">01/04(四)</li>
+            <li className="info date">01/05(五)</li>
+          </ul>
+          <ul className="row">
+            <li className="info">--</li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+          </ul>
+          <ul className="row">
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info on_sale">
+              <span className="price">$12,300</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+          </ul>
+          <ul className="row">
+            <li className="info">查看</li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+          </ul>
+          <ul className="row">
+            <li className="info">查看</li>
+            <li className="info">查看</li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info on_sale">
+              <span className="price">$12,300</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+          </ul>
+          <ul className="row">
+            <li className="info">查看</li>
+            <li className="info">查看</li>
+            <li className="info">查看</li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info on_sale">
+              <span className="price">$12,300</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+          </ul>
+          <ul className="row">
+            <li className="info">查看</li>
+            <li className="info">查看</li>
+            <li className="info">查看</li>
+            <li className="info">查看</li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+          </ul>
+          <ul className="row">
+            <li className="info">查看</li>
+            <li className="info">查看</li>
+            <li className="info">查看</li>
+            <li className="info">查看</li>
+            <li className="info">查看</li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+            <li className="info">
+              <span className="price">$15,568</span>起
+            </li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FzTable;
